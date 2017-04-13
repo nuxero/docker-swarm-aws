4d54eb1f741b2579469371b4a572c857a84a5aa1
Hector Zelaya <inge.zelaya@gmail.com>
2017-04-12 18:59:30 -0600

    added funcionality for removing replaced managers from cluster and improve README.md

--- README.md
+++ README.md
@@ -1,18 +1,21 @@
 # Docker Swarm on AWS
 This repo provisions a Docker Swarm Cluster on AWS using Packer and Terraform.
 
-First create the ami using packer and provide the region variable. The packer script uploads two files *docker.conf* and *init.js*. The first is for customize the docker service and the latter is a nodejs script for initialize the cluster/join nodes to it.
-
-`packer build -var 'region=us-east-1' docker.json`
-
-Write down the ami id generated.
-
-The terraform scripts don't handle credentials, instead its use env variables, so set your variables like this:
+First export yout credentials and create the ami using packer. Provide the region variable when executing packer. The packer script uploads two files *docker.conf* and *init.py*. The first is for customize the docker service and the latter is a python script for initialize the cluster and add nodes to it.
 
 ```
 export AWS_ACCESS_KEY_ID=your-access-key-here
 export AWS_SECRET_ACCESS_KEY=your-secret-access-key-here
 export AWS_DEFAULT_REGION=your-region
+
+packer build -var 'region=us-east-1' docker.json
+```
+
+Write down the ami id generated.
+
+Set the terraform variables like this:
+
+```
 export TF_VAR_manager_count=how-many-managers-do-you-want Ej: 1, 3, 5 (odd numbers recommended)
 export TF_VAR_worker_count=how-many-workers-do-you-want Ej: 1,5,10,1000
 export TF_VAR_key=your-key
@@ -20,7 +23,7 @@ export TF_VAR_ec2_ami=the-ami-key-generated-with-packer
 export TF_VAR_key_local_path=path-to-the-key-pair-for-ssh
 ```
 
-Now you can provision the infrastructure with Terraform
+Also check all the variables on the `variables.tf` file and modify as you wish. Now you can provision the infrastructure with Terraform
 
 `terraform apply`
 
@@ -37,7 +40,8 @@ Currently there are no outputs defined so you should login to AWS console to get
 
 # TO-DO
 * ~~Think about how to initialize cluster and how worker nodes will automagically join to it~~
-* Improve cluster initializer/join-nodes script
+* ~~Improve cluster initializer/join-nodes script~~
+* Remove workers from cluster when replaced
 * Deploy some CI/CD tools on cluster (Jenkins?)
 * Add a Docker registry
 * Add a proxy for accessing containers (Docker Flow Proxy?)
--- init.py
+++ init.py
@@ -1,57 +1,89 @@
+####    IMPORTS
 import boto3
 import os
 from subprocess import run
 from subprocess import call
 from subprocess import PIPE
 
+####    INITIALIZE VARIABLES
 ec2 = boto3.client('ec2')
 role = os.environ['ROLE']
-instance = os.environ['INSTANCE']
+current_instance = os.environ['INSTANCE']
 
-def add_tag():
+####    SOME UTILITIES
+def add_tag(tags):
     response_tags = ec2.create_tags(
         Resources=[
-            instance
+            current_instance
         ],
-        Tags=[
-            {
-                'Key': 'Init',
-                'Value': 'true'
-            }
-        ]
+        Tags=tags
     )
     print(response_tags)
 
-response = ec2.describe_instances(
-    Filters=[
-        {
-            'Name': 'tag:Name',
-            'Values': ['SwarmManager']
-        },
-        {
-            'Name': 'instance-state-name',
-            'Values': ['running']
-        },
-        {
-            'Name': 'tag:Init',
-            'Values': ['true']
-        }
-    ]
-)
+def describe_managers(instance_state):
+    response = ec2.describe_instances(
+        Filters=[
+            {
+                'Name': 'tag:Name',
+                'Values': ['SwarmManager']
+            },
+            {
+                'Name': 'instance-state-name',
+                'Values': instance_state
+            },
+            {
+                'Name': 'tag:Init',
+                'Values': ['true']
+            }
+        ]
+    )
+    return response
 
-instances = 0
-for res in response['Reservations']:
-    instances += len(res['Instances'])
+####    QUERY MANAGERS
+managers_running = describe_managers(['running'])
+managers_replaced = describe_managers(['shutting-down','stopped','terminated','stopping'])
 
-print(instances)
-if instances > 0 or role == 'worker':
-    manager = response['Reservations'][0]['Instances'][0]['PrivateIpAddress']
-    call(["echo","'Here I join to the cluster {}'".format(manager)])
-    token = run(["docker","-H {}:2376".format(manager),"swarm","join-token","-q","{}".format(role)],stdout=PIPE)
-    call(['docker','swarm','join','{}:2377'.format(manager),'--token','{}'.format(token.stdout.decode('utf-8').replace('\n',''))])
+instances_count = 0
+for mgr in managers_running['Reservations']:
+    instances_count += len(mgr['Instances'])
+print(instances_count)
+
+replaced_instances_count = 0
+replaced_instances = []
+for mgr in managers_replaced['Reservations']:
+    replaced_instances_count += len(mgr['Instances'])
+    for instance in mgr['Instances']:
+        replaced_instances.append(instance)
+print(replaced_instances_count)
+
+####    INITIALIZE/JOIN CLUSTER
+if instances_count > 0 or role == 'worker':
+    ####    JOIN CLUSTER
+    manager = managers_running['Reservations'][0]['Instances'][0]['PrivateIpAddress'] # any running manager
+    call(["echo","'Here I join to the cluster {}'".format(manager)]) # a friendly message for debugging
+    token = run(["docker","-H {}:2376".format(manager),"swarm","join-token","-q",role],stdout=PIPE) # get the token
+    call(['docker','swarm','join','{}:2377'.format(manager),'--token',token.stdout.decode('utf-8').replace('\n','')]) # join to cluster
+
+    ####    ADD TAGS
     if role == 'manager':
-        add_tag()
+        hostname = run(['hostname'],stdout=PIPE)
+        add_tag([{'Key': 'Init','Value': 'true'},{'Key': 'Hostname','Value': hostname.stdout.decode('utf-8').replace('\n','')}])
+
+    ####    CLEAN NODES
+    if replaced_instances_count > 0 and role == 'manager':
+        ## docker demote node && docker rm node
+        for instance in replaced_instances:
+            for tag in instance['Tags']:
+                if tag['Key'] == 'Hostname':
+                    call(["docker","node","demote",tag['Value']])
+                    call(["docker","node","rm",tag['Value']])
+
+
 else:
+    ####    INIT CLUSTER
     call(["echo","'Here I init the cluster'"])
     call(["docker","swarm","init"])
-    add_tag()
+
+    ####    ADD TAGS
+    hostname = run(['hostname'],stdout=PIPE)
+    add_tag([{'Key': 'Init','Value': 'true'},{'Key': 'Hostname','Value': hostname.stdout.decode('utf-8').replace('\n','')}])
